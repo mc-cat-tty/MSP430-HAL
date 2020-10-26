bdcbbeb2d7c7b3ae01f0f3190f0ee51b246a3b3d
Francesco Mecatti <francesco.mecatti@github.com>
2020-10-26 20:26:51 +0100

    Added examples

---
+++ Timer/examples/no_multiple_timers_demo2.cpp
@@ -0,0 +1,34 @@
+//
+// File no_multiple_timers_demo2.cpp
+// Author Francesco Mecatti
+//
+#include <msp430.h>
+#include <Timer.h>
+
+// TODO: All --> demonstrate that if t2 is created and then started, the wait method doesn't work
+
+void setup(void);
+void toggle_green(void);
+
+int main(void) {
+  WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+  setup();
+  TimerA::Timer t1;  // Multiple timers are not allowed
+  t1.start();  // Instead, use multiple callback functions (attached to one timer)
+  TimerA::Timer t2;  // This timer is configured
+  // t2.stop();  // This method is not executed
+  while (1) {
+      t2.wait(2000);
+      toggle_green();
+  }
+}
+
+void setup (void) {
+  // *** Green Led configuration ***
+  P4DIR |= BIT7;  // Green Led output direction
+  P4OUT &= ~BIT7;  // Clear Green Led output
+}
+
+void toggle_green (void) {
+  P4OUT ^= BIT7;  // Toggle Green Led
+}
