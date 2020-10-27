e490ff085d952845fdc9bcb979ffd523707cd730
Francesco Mecatti <francesco.mecatti@github.com>
2020-10-27 18:58:04 +0100

    Added function interfaces to Debug.h

---
+++ Debug/src/Debug.cpp
@@ -0,0 +1,9 @@
+//
+// File Debug.cpp
+// Author Francesco Mecatti
+//
+#include "Debug.h"
+
+using namespace Debug;
+
+void setupGreenLed(void) {}
--- Debug/src/Debug.h
+++ Debug/src/Debug.h
@@ -7,4 +7,27 @@
 
 #include <msp430.h>
 
-#endif DEBUG_H
+// Use it if the function contains dead times
+#define __wait __attribute__((optimize("O0")))
+
+namespace Debug  {
+    void setupGreenLed(void);
+    void setupRedLed(void);
+    void turnOnGreenLed(void);
+    void turnOffGreenLed(void);
+    void turnOnRedLed(void);
+    void turnOffRedLed(void);
+    __wait void blinkGreenLed(void);
+    __wait void blinkRedLed(void);
+
+    // When called turns on green led. Place where you want to valuate if that point is reached
+    void reachedA(void);
+
+    // When reached turns on red led. Place where you want to valuate if that point is reached
+    void reachedB(void);
+
+    // Output the value of num by blinking green led that number of times. Red led blinks before a new "transmission"
+    __wait void value(const short unsigned int num);
+}
+
+#endif  // DEBUG_H
