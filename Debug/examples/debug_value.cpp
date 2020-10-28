f2f45e8f73298617b63e113b8f9dac6e47d43392
Francesco Mecatti <francesco.mecatti@github.com>
2020-10-28 01:15:27 +0100

    debug_value.ccp error corrected 11:10

--- Debug/examples/debug_value.cpp
+++ Debug/examples/debug_value.cpp
@@ -8,7 +8,7 @@
 int main(void) {
   WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
   for (int i = 0; i < 10; i+=2) {
-    if (!i%2)
+    if (!(i%2))
       // I want to know the value of i, but UART (or every other peripheral is not available)
       Debug::value(i);  // The "transmission" starts with red led blink and then
                         // the number of green led's blink represent num (passed arg)
